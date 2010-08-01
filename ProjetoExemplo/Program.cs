3718e2acf112ddd3e05b880953fbba0e2a49bf30
Samuel B. Oldra <samuel.oldra@gmail.com>
2010-08-01 00:00:00 -0300

    Primeiro commit

---
+++ ProjetoExemplo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ninject;
+
+namespace ProjetoExemplo
+{
+    public interface ILogger
+    {
+        void Write(string message);
+    }
+
+    public class ConsoleLogger : ILogger
+    {
+        public ConsoleLogger(DatabaseLogger data)
+        {
+            data.Write("Instanciou por mágica!");
+        }
+
+        public void Write(string message)
+        {
+            Console.WriteLine(String.Format("Message: {0}", message));
+            Console.WriteLine("Target: ConsoleLogger");
+        }
+    }
+
+    public class DatabaseLogger : ILogger
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine(String.Format("Message: {0}", message));
+            Console.WriteLine("Target: Database");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKernel kernel = new StandardKernel();
+            ILogger logger = kernel.Get<DatabaseLogger>("teste");
+            logger.Write("teste");
+            Console.Read();
+        }
+    }
+}
